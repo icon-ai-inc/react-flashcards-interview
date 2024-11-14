0030c98ef1c435bb8b3100892c1dd05e689a1dc9
Caleb-Tan <calebtan23@gmail.com>
2024-11-14 13:27:34 -0500

    update

--- src/client/src/App.jsx
+++ src/client/src/App.jsx
@@ -1,29 +1,27 @@
 import "./App.css";
-import { Routes, Route, BrowserRouter as Router, } from "react-router-dom";
-import React, {useEffect, useState} from "react";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 
 const Placeholder = () => {
-    const [users, setUsers] = useState([]);
+  const [flashcards, setFlashcards] = useState([]);
 
-    useEffect(() => {
-        fetch('/api/users').then( async (user) => {
-            const usersReturned = await user.json();
-            setUsers(Object.values(usersReturned)[0])
-        })
-    }, []);
+  useEffect(() => {
+    fetch("/api/flashcards").then(async (flashcards) => {
+      const flashcardsReturned = await flashcards.json();
+      setFlashcards(flashcardsReturned);
+    });
+  }, []);
 
-    return <div>{users.join(',')}</div>
+  return <div>{users.join(",")}</div>;
 };
 
 function App() {
-
-
   return (
     <div className="App">
       <Router>
-          <Routes>
-              <Route path="/flashcards" element={<Placeholder/>} />
-          </Routes>
+        <Routes>
+          <Route path="/flashcards" element={<Placeholder />} />
+        </Routes>
       </Router>
     </div>
   );
--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -1,21 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const { db } = require('../adapters/postgres');
+const { db } = require("../adapters/postgres");
 
 // Sample code to show syntax, not relevant to application but should help get started
 
 /* GET users listing. */
-router.get("/users", async function(req, res, next) {
-  const dataResults = await db.query('select * from flashcards');
-  res.send({ users: ["joe2", "bernie", "tulsi", "donald", "bill", dataResults[0]?.definition] });
+router.get("/flashcards", async function (req, res, next) {
+  // const dataResults = await db.query("fill in query here");
+  res.send({
+    flashcards: ["test", "test2", "test3", "test4", "test5"],
+  });
 });
 
 /* POST users listing. */
-router.post("/users", function(req, res, next) {
+router.post("/users", function (req, res, next) {
   const { username, password } = req.body;
   console.log('POST "users" route hit');
   // create a user
-  res.sendStatus(200)
+  res.sendStatus(200);
 });
 
 // Begin coding here
